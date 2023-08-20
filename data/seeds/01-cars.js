37a90d7fa83d003fb1d03109b0b702e8c8203d13
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-08-20 14:35:33 -0400

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+  {
+    vin: "1111111111111",
+    make: "toyota",
+    model: "prius",
+    mileage: 21500,
+    title: "clean",
+    transmission: "manual",
+  },
+  {
+    vin: "1111111111111",
+    make: "toyota",
+    model: "corolla",
+    mileage: 11500,
+    title: "salvage",
+  },
+  {
+    vin: "1111111111111",
+    make: "ford",
+    model: "focus",
+    mileage: 2500,
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(cars);
+};
